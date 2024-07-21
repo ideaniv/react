eb269dcc083e133d8ee2ef8628afd356432c0e50
khanna01 <govmfkdtm@gmail.com>
2024-07-21 15:50:07 +0900

    main page left aside

---
+++ hn/basic-css-project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./components/Header";
+import LeftAside from "./components/Left-aside";
+
+function App() {
+  return (
+      <>
+        <Header />
+        <LeftAside />
+      </>
+  );
+}
+
+export default App;
