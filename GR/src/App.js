5149fc815ea65126982c2e139c33b6e179af1f35
Green-JEONG <th2gr22n@gmail.com>
2024-07-23 20:18:52 +0900

    [add] pointPageSpent-2

--- GR/src/App.js
+++ GR/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/common/header';
 import PointPageSpent from './components/PointPageSpent';
+
 function App() {
   return <>
   <Header />
--- GR/src/components/common/header.jsx
+++ GR/src/components/common/header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import logo from "../../images/Header/image_19_1.svg";
-import userIcon from "../../images/common/Frame.svg";
-import Alarm from "../../images/Header/Rectangle.svg";
+import userIcon from "../../images/Header/Frame.svg";
 
 export default function Header() {
   return (
     <header
+      className="test"
       style={{
         display: "flex",
         alignItems: "center",
@@ -30,7 +30,26 @@ export default function Header() {
           OpenSource Suppoter
         </p>
       </div>
-      <img src={userIcon} />
+      <div
+        style={{
+          position: "relative",
+        }}
+      >
+        <img src={userIcon} width={20} />
+        <div
+          style={{
+            backgroundColor: "#006EED",
+            width: "10px",
+            height: "10px",
+            border: "2px solid #161B22",
+            borderRadius: "50%",
+            position: "absolute",
+            top: 0,
+            right: 0,
+            transform: "translateX(7px) translateY(-4px)",
+          }}
+        ></div>
+      </div>
     </header>
   );
 }
