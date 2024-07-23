04910118379015b4f10928170c718d080c538a8f
khanna01 <govmfkdtm@gmail.com>
2024-07-23 20:21:32 +0900

    point page

--- hn/basic-css-project/src/App.js
+++ hn/basic-css-project/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
 import Header from "./components/Header";
 import LeftAside from "./components/Left-aside";
+import RightAside from './components/Right-aside'
+import Main from './components/Main'
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Header />
-      <LeftAside />
-    </>
+        <div className="body-container">
+        <LeftAside />
+        <Main />
+        <RightAside />
+        </div>
+    </div>
   );
 }
 
