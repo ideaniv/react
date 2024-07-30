142fa28b7016dc8a5851dc1c98c9b61bad4a561f
Kimkyeongbeom <bibix321@naver.com>
2024-07-30 22:13:39 +0900

    updated : react-router-dom 및 아토믹디자인 적용

--- hn/basic-css-project/tailwind/src/components/Header.jsx
+++ hn/basic-css-project/tailwind/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import logo from '../assets/img/image_19_1.png'
 import userIcon from '../assets/img/Frame.png'
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    const navigate = useNavigate();
+
     return (
         <header className="sticky top-0 flex justify-between items-center bg-[#161B22] px-[24px] py-[11px]">
             <div className="flex items-center">
-                <img src={logo} />
+                <img src={logo} className='hover:cursor-pointer' onClick={()=>navigate('/')}/>
                 <p className="text-white ml-[10px]">
                     OpenSource Suppoter
                 </p>
--- hn/basic-css-project/tailwind/src/components/Right-aside.jsx
+++ hn/basic-css-project/tailwind/src/components/Right-aside.jsx
@@ -3,17 +3,19 @@ import bookIcon from '../assets/img/book.svg'
 import globeIcon from '../assets/img/globe.svg'
 import packageIcon from '../assets/img/package.svg'
 import starIcon from '../assets/img/star.svg'
+import { useNavigate } from 'react-router-dom'
 
 export default function RightAside() {
+    const navigate = useNavigate();
     return (
         <section className="flex flex-col justify-around bg-[#0d1117] border-l border-l-solid border-l-white/[0.3] py-[80px] px-[46px]">
-            <nav className="menu_item"><img className="mr-[4px]" src={homeIcon} alt="" />Your profile</nav>
-            <nav className="menu_item"><img className="mr-[4px]" src={bookIcon} alt="" />Your Repositories</nav>
-            <nav className="menu_item"><img className="mr-[4px]" src={globeIcon} alt="" />Your Links</nav>
-            <nav className="menu_item"><img className="mr-[4px]" src={packageIcon} alt="" />Your Points</nav>
-            <nav className="menu_item"><img className="mr-[4px]" src={starIcon} alt="" />Your Ranks</nav>
-            <nav className="menu_item py-[35px] border-y border-y-solid border-y-[#21262D] ">Opensource Licenses</nav>
-            <nav className="menu_item text-[#FF0000]/[0.53]">Log out</nav>
+            <nav className="menu_item hover:cursor-pointer"><img className="mr-[4px]" src={homeIcon} alt="" />Your profile</nav>
+            <nav className="menu_item hover:cursor-pointer"><img className="mr-[4px]" src={bookIcon} alt="" />Your Repositories</nav>
+            <nav className="menu_item hover:cursor-pointer"><img className="mr-[4px]" src={globeIcon} alt="" />Your Links</nav>
+            <nav className="menu_item hover:cursor-pointer" onClick={()=>navigate('/point')}><img className="mr-[4px]" src={packageIcon} alt="" />Your Points</nav>
+            <nav className="menu_item hover:cursor-pointer"><img className="mr-[4px]" src={starIcon} alt="" />Your Ranks</nav>
+            <nav className="menu_item hover:cursor-pointer py-[35px] border-y border-y-solid border-y-[#21262D] ">Opensource Licenses</nav>
+            <nav className="menu_item hover:cursor-pointer text-[#FF0000]/[0.53]">Log out</nav>
         </section>
     )
 }
